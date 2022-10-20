a5989a2f2388d3f436b310838cb592a926fa8349
alaanegm <43115408+alaanegm@users.noreply.github.com>
2022-10-20 15:21:36 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Screen from './component/Screen'
+import Button from './component/Button'
+import Context from "./context/Context";
 function App() {
+  const btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "x"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Context>
+    <div className="wrapper">
+          <Screen />
+          <div className="buttonBox">
+          {btnValues.flat().map((btn, i) => (
+            <Button
+              value={btn}
+              key={i}
+            />
+          ))}
+          </div>
+      
     </div>
+   </Context>
   );
 }
 
